d73296598846928efef2870f76e2005242eafebc
Santiago Chavarro <santichav417@gmail.com>
2024-10-18 01:15:34 -0500

    create or CRUD for each table class, we are ready to create querys

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
 from app.routes import router
-from pydantic import BaseModel, Field
-from datetime import date
-
-from app.models import CourseCreate
-
 app = FastAPI()
-
 app.include_router(router)
+app.title = "API Telepizza"
