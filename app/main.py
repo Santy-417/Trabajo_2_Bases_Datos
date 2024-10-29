901b44757b8ca9e23d9a7f9d1209c5946acb2f76
Santiago Chavarro <santichav417@gmail.com>
2024-10-28 23:09:23 -0500

    Improvement: Update welcome message

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.routes import router
+
 app = FastAPI()
+
 app.include_router(router)
+
 app.title = "API Telepizza"
+
+@app.get("/")
+async def root():
+    return {"message": "Bienvenido a la API de Telepizza"}
